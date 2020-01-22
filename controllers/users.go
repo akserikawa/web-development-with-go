e076744cd7a78ae1e6a716a9cb42283cd1fe735d
Akira <akserikawa@gmail.com>
2020-01-22 14:53:07 +0100

    7.3.2 Moving the sign up page code

--- controllers/users.go
+++ controllers/users.go
@@ -1,3 +1,23 @@
 package controllers
 
-type Users struct{}
+import (
+	"net/http"
+
+	"lenslocked.com/views"
+)
+
+type Users struct {
+	NewView *views.View
+}
+
+func (u *Users) New(w http.ResponseWriter, r *http.Request) {
+	if err := u.NewView.Render(w, nil); err != nil {
+		panic(err)
+	}
+}
+
+func NewUsers() *Users {
+	return &Users{
+		NewView: views.NewView("bootstrap-4", "views/users/new.gohtml"),
+	}
+}
