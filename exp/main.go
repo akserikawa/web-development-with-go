2b133e3b00a45c68c002f8b97d06b8a6e0fb7ce0
Akira Serikawa <akserikawa@gmail.com>
2020-02-02 22:00:23 +0100

    8.4.1 Installing GORM and connecting to a database

--- exp/main.go
+++ exp/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 
-	_ "github.com/lib/pq"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 const (
@@ -18,33 +18,9 @@ const (
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-	db, err := sql.Open("postgres", psqlInfo)
+	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("Successfully connected!")
-
-	var id, orderID, orderAmount int
-	var name, email, orderDescription string
-	rows, err := db.Query(`
-		SELECT users.id, users.name, users.email, orders.id, orders.amount, orders.description
-		FROM users
-		INNER JOIN orders
-		ON users.id = orders.user_id`)
-	if err != nil {
-		panic(err)
-	}
-
-	for rows.Next() {
-		rows.Scan(&id, &name, &email, &orderID, &orderAmount, &orderDescription)
-		fmt.Println("ID:", id, "Name:", name, "Email:", email,
-			"Order ID:", orderID, "Amount:", orderAmount, "Description:", orderDescription)
-	}
-
-	db.Close()
+	defer db.Close()
 }
