56b2f2b676412e9675c8612cc0ad95ea269c0f37
Akira Serikawa <akserikawa@gmail.com>
2020-02-02 21:56:08 +0100

    8.3.7 Querying related records

--- exp/main.go
+++ exp/main.go
@@ -29,24 +29,22 @@ func main() {
 
 	fmt.Println("Successfully connected!")
 
-	var id int
-	for i := 1; i < 6; i++ {
-		userID := 1
-		if i > 3 {
-			userID = 2
-		}
-		amount := 1000 * i
-		description := fmt.Sprintf("USB-C Adapter x%d", i)
-
-		err = db.QueryRow(`
-			INSERT INTO orders (user_id, amount, description)
-			VALUES ($1, $2, $3)
-			RETURNING id`,
-			userID, amount, description).Scan(&id)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println("Created an order with the ID:", id)
+	var id, orderID, orderAmount int
+	var name, email, orderDescription string
+	rows, err := db.Query(`
+		SELECT users.id, users.name, users.email, orders.id, orders.amount, orders.description
+		FROM users
+		INNER JOIN orders
+		ON users.id = orders.user_id`)
+	if err != nil {
+		panic(err)
 	}
+
+	for rows.Next() {
+		rows.Scan(&id, &name, &email, &orderID, &orderAmount, &orderDescription)
+		fmt.Println("ID:", id, "Name:", name, "Email:", email,
+			"Order ID:", orderID, "Amount:", orderAmount, "Description:", orderDescription)
+	}
+
 	db.Close()
 }
