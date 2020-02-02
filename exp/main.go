02060a413c2587f2927a652cd80b8559ac91ec36
Akira Serikawa <akserikawa@gmail.com>
2020-02-02 21:41:35 +0100

    8.3.5 Querying multiple records with database/sql

--- exp/main.go
+++ exp/main.go
@@ -31,14 +31,19 @@ func main() {
 
 	var id int
 	var name, email string
-	row := db.QueryRow(`
+	rows, err := db.Query(`
 	  SELECT id, name, email
 	  FROM users
-	  WHERE id=$1`, 1)
-	err = row.Scan(&id, &name, &email)
+	  WHERE email = $1
+	  OR ID > $2`,
+		"jon@calhoun.io", 3)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("ID:", id, "Name:", name, "Email:", email)
+
+	for rows.Next() {
+		rows.Scan(&id, &name, &email)
+		fmt.Println("ID:", id, "Name:", name, "Email:", email)
+	}
 	db.Close()
 }
