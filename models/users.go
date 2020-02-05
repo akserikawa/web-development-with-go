383e49a024c79aad437ebf3392895303b0acee5d
Akira Serikawa <akserikawa@gmail.com>
2020-02-05 19:19:58 +0100

    9.2 Creating the UserService interface and querying for users

--- models/users.go
+++ models/users.go
@@ -7,10 +7,25 @@ import (
 
 type User struct {
 	gorm.Model
-	Name  string
+	Name string
 	Email string `gorm:"not null;unique_index"`
 }
 
 type UserService struct {
 	db *gorm.DB
+}
+
+func NewUserService(connectionInfo string) (*UserService, error) {
+	db, err := gorm.Open("postgres", connectionInfo)
+	if err != {
+		return nil, err
+	}
+	db.LogMode(true)
+	return &UserService{
+		db: db,
+	}, nil
+}
+
+func (us *UserService) Close() error {
+	return us.db.Close()
 }
