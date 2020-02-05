1beadeb5d26a25cdb1d0eb0dc70c39e2ce228c08
Akira Serikawa <akserikawa@gmail.com>
2020-02-05 19:31:05 +0100

    9.2.1 Creating the UserService interface and querying for users

--- exp/main.go
+++ exp/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 
+	"lenslocked.com/models"
+
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
@@ -15,49 +17,20 @@ const (
 	dbname   = "lenslocked.com"
 )
 
-type User struct {
-	gorm.Model
-	Name   string
-	Email  string `gorm:"not null;unique_index"`
-	Orders []Order
-}
-
-type Order struct {
-	gorm.Model
-	UserID      uint
-	Amount      int
-	Description string
-}
-
 func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
+	us, err := models.NewUserService(psqlInfo)
 	db, err := gorm.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
+	us.DestructiveReset()
 
-	db.LogMode(true)
-	db.AutoMigrate(&User{}, &Order{})
-
-	var user User
-	db.Preload("Orders").First(&user)
-	if db.Error != nil {
-		panic(db.Error)
+	user, err := us.ByID(1)
+	if err != nil {
+		panic(err)
 	}
-	fmt.Println("Email:", user.Email)
-	fmt.Println("Number of orders:", len(user.Orders))
-	fmt.Println("Orders:", user.Orders)
-}
-
-func createOrder(db *gorm.DB, user User, amount int, desc string) {
-	db.Create(&Order{
-		UserID:      user.ID,
-		Amount:      amount,
-		Description: desc,
-	})
-	if db.Error != nil {
-		panic(db.Error)
-	}
+	fmt.Println(user)
 }
