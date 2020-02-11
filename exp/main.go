1304fff393e30a2f62c593805674ef2350ef7ff8
Akira <akserikawa@gmail.com>
2020-02-11 14:15:24 +0100

    11.5 Generating remember tokens

--- exp/main.go
+++ exp/main.go
@@ -3,12 +3,8 @@ package main
 import (
 	"fmt"
 
-	"github.com/dchest/uniuri"
-
-	"lenslocked.com/models"
-
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"lenslocked.com/rand"
 )
 
 const (
@@ -20,30 +16,6 @@ const (
 )
 
 func main() {
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
-	us, err := models.NewUserService(psqlInfo)
-	db, err := gorm.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-	us.DestructiveReset()
-
-	for i := 18; i < 35; i++ {
-		user := models.User{
-			Name:  uniuri.NewLen(4) + " " + uniuri.NewLen(8),
-			Email: uniuri.New() + "@gmail.com",
-			Age:   int8(i),
-		}
-		if err := us.Create(&user); err != nil {
-			panic(err)
-		}
-	}
-
-	foundUsers, err := us.InAgeRange(22, 32)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(foundUsers)
+	fmt.Println(rand.String(10))
+	fmt.Println(rand.RememberToken())
 }
