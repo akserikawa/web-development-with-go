88e4d06644748ef3d87c93f4ac33934303c98c70
Akira Serikawa <akserikawa@gmail.com>
2020-02-12 20:07:04 +0100

    11.8.2 Restricting page access

--- controllers/users.go
+++ controllers/users.go
@@ -107,9 +107,15 @@ func (u *Users) signIn(w http.ResponseWriter, user *models.User) error {
 }
 
 func (u *Users) CookieTest(w http.ResponseWriter, r *http.Request) {
-	cookie, err := r.Cookie("email")
+	cookie, err := r.Cookie("remember_token")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
-	fmt.Fprintln(w, "Email is: ", cookie.Value)
+	user, err := u.us.ByRemember(cookie.Value)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Fprintln(w, user)
 }
