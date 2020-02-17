c7fab67e05f42f532ee056328a907f32464aa0cc
Akira <akserikawa@gmail.com>
2020-02-17 13:01:08 +0100

    12.5.2 Ensuring remember tokens are set on create

--- models/users.go
+++ models/users.go
@@ -209,16 +209,9 @@ func (uv *userValidator) ByRemember(token string) (*User, error) {
 }
 
 func (uv *userValidator) Create(user *User) error {
-	if user.Remember == "" {
-		token, err := rand.RememberToken()
-		if err != nil {
-			return err
-		}
-		user.Remember = token
-	}
-
 	err := runUserValFns(user,
 		uv.bcryptPassword,
+		uv.setRememberIfUnset,
 		uv.hmacRemember)
 	if err != nil {
 		return err
@@ -271,5 +264,17 @@ func (uv *userValidator) hmacRemember(user *User) error {
 		return nil
 	}
 	user.RememberHash = uv.hmac.Hash(user.Remember)
+	return nil
+}
+
+func (uv *userValidator) setRememberIfUnset(user *User) error {
+	if user.Remember != "" {
+		return nil
+	}
+	token, err := rand.RememberToken()
+	if err != nil {
+		return err
+	}
+	user.Remember = token
 	return nil
 }
