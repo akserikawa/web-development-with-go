6bd49d2a53d74015c1c5d033979dc156c5358640
Akira <akserikawa@gmail.com>
2020-02-18 11:49:33 +0100

    12.9 Validating and normalizing remember tokens

--- models/users.go
+++ models/users.go
@@ -20,9 +20,10 @@ var (
 	ErrEmailRequired     = errors.New("models: email address is required")
 	ErrEmailInvalid      = errors.New("models: email address is not valid")
 	ErrEmailTaken        = errors.New("models: email address is already taken")
-	ErrPasswordTooShort  = errors.New("models: password must " +
-		"be at least 8 characters long")
-	ErrPasswordRequired = errors.New("models: password is required")
+	ErrPasswordTooShort  = errors.New("models: password must be at least 8 characters long")
+	ErrPasswordRequired  = errors.New("models: password is required")
+	ErrRememberRequired  = errors.New("models: remember token is required")
+	ErrRememberTooShort  = errors.New("models: remember token must be at least 32 bytes")
 )
 
 var userPwPepper = "secret-random-string"
@@ -230,7 +231,9 @@ func (uv *userValidator) Create(user *User) error {
 		uv.bcryptPassword,
 		uv.passwordHashRequired,
 		uv.setRememberIfUnset,
+		uv.rememberMinBytes,
 		uv.hmacRemember,
+		uv.rememberHashRequired,
 		uv.normalizeEmail,
 		uv.requireEmail,
 		uv.emailFormat,
@@ -246,7 +249,9 @@ func (uv *userValidator) Update(user *User) error {
 		uv.passwordMinLength,
 		uv.bcryptPassword,
 		uv.passwordHashRequired,
+		uv.rememberMinBytes,
 		uv.hmacRemember,
+		uv.rememberHashRequired,
 		uv.normalizeEmail,
 		uv.requireEmail,
 		uv.emailFormat,
@@ -387,6 +392,27 @@ func (uv *userValidator) passwordRequired(user *User) error {
 func (uv *userValidator) passwordHashRequired(user *User) error {
 	if user.PasswordHash == "" {
 		return ErrPasswordRequired
+	}
+	return nil
+}
+
+func (uv *userValidator) rememberMinBytes(user *User) error {
+	if user.Remember == "" {
+		return nil
+	}
+	n, err := rand.NBytes(user.Remember)
+	if err != nil {
+		return err
+	}
+	if n < 32 {
+		return ErrRememberTooShort
+	}
+	return nil
+}
+
+func (uv *userValidator) rememberHashRequired(user *User) error {
+	if user.RememberHash == "" {
+		return ErrRememberRequired
 	}
 	return nil
 }
