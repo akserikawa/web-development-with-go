e60d11cbf57024053d04c0228db698347a2082f7
Akira <akserikawa@gmail.com>
2020-02-19 15:10:07 +0100

    14.8.2 Parsing the edit gallery form

--- main.go
+++ main.go
@@ -52,12 +52,14 @@ func main() {
 	newGallery := requireUserMiddleware.Apply(galleriesController.New)
 	createGallery := requireUserMiddleware.ApplyFn(galleriesController.Create)
 	editGallery := requireUserMiddleware.ApplyFn(galleriesController.Edit)
+	updateGallery := requireUserMiddleware.ApplyFn(galleriesController.Update)
 
 	router.Handle("/galleries/new", newGallery).Methods("GET")
 	router.HandleFunc("/galleries", createGallery).Methods("POST")
 	router.HandleFunc("/galleries/{id:[0-9]+}", galleriesController.Show).
 		Methods("GET").Name(controllers.ShowGallery)
 	router.HandleFunc("/galleries/{id:[0-9]+}/edit", editGallery).Methods("GET")
+	router.HandleFunc("/galleries/{id:[0-9]+}/update", updateGallery).Methods("POST")
 
 	log.Println("Server listening on http://localhost:3000")
 	log.Fatal(http.ListenAndServe(":3000", router))
