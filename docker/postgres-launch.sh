22789217b81688cb82d5b4f75008b0d9fb409be6
Akira Serikawa <akserikawa@gmail.com>
2020-03-02 18:36:13 +0100

    Fix posgres docker image tag

--- docker/postgres-launch.sh
+++ docker/postgres-launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --name pg-docker -e POSTGRES_PASSWORD=postgres -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres:12-alpine
+docker run --rm --name pg-docker -e POSTGRES_PASSWORD=postgres -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres:12
