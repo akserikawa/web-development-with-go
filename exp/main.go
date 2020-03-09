8ae40b14fa5df99b145631035e6efa73a6d44630
Akira Serikawa <akserikawa@gmail.com>
2020-03-09 22:46:16 +0100

    Fix exp code

--- exp/main.go
+++ exp/main.go
@@ -18,11 +18,11 @@ func main() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
-	services, err := models.NewServices(psqlInfo)
+	services, err := models.NewServices("postgres", psqlInfo)
 
 	if err != nil {
 		panic(err)
 	}
 	defer services.Close()
-	services.DestructiveReset()
+	// services.DestructiveReset()
 }
